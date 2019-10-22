9189ec0f72497b76a63362b4ed73d29f6d7e6238
Ernesto Serrano <info@ernesto.es>
2019-10-22 22:32:48 +0200

    Set final test case

--- code/run.sh
+++ code/run.sh
@@ -6,10 +6,10 @@
 
 
 
-python3 genetic.py --individuals 16 --crossover-one-point --random-mutation 		> results/results_16_1_random_$1.txt
-python3 genetic.py --individuals 16 --crossover-one-point --no-random-mutation 	> results/results_16_1_one_$1.txt
-python3 genetic.py --individuals 16 --crossover-two-points --random-mutation 	> results/results_16_2_random_$1.txt
-python3 genetic.py --individuals 16 --crossover-two-points --no-random-mutation  > results/results_16_2_one_$1.txt
+# python3 genetic.py --individuals 16 --crossover-one-point --random-mutation 		> results/results_16_1_random_$1.txt
+# python3 genetic.py --individuals 16 --crossover-one-point --no-random-mutation 	> results/results_16_1_one_$1.txt
+# python3 genetic.py --individuals 16 --crossover-two-points --random-mutation 	> results/results_16_2_random_$1.txt
+# python3 genetic.py --individuals 16 --crossover-two-points --no-random-mutation  > results/results_16_2_one_$1.txt
 
 # python3 genetic.py --individuals 32 --crossover-one-point --random-mutation		> results/results_32_1_random_$1.txt
 # python3 genetic.py --individuals 32 --crossover-one-point --no-random-mutation		> results/results_32_1_one_$1.txt
@@ -26,10 +26,21 @@ python3 genetic.py --individuals 16 --crossover-two-points --no-random-mutation 
 for i in `seq $1` ; do
 	echo "Running $i execution..."
     
-    python3 genetic.py --individuals 16 --crossover-one-point --random-mutation 		> results/results_16_1_random_$1.txt
-	python3 genetic.py --individuals 16 --crossover-one-point --no-random-mutation 	> results/results_16_1_one_$1.txt
-	python3 genetic.py --individuals 16 --crossover-two-points --random-mutation 	> results/results_16_2_random_$1.txt
+    python3 genetic.py --individuals 16 --crossover-one-point --random-mutation > results/results_16_1_random_$1.txt
+	python3 genetic.py --individuals 16 --crossover-one-point --no-random-mutation > results/results_16_1_one_$1.txt
+	python3 genetic.py --individuals 16 --crossover-two-points --random-mutation > results/results_16_2_random_$1.txt
 	python3 genetic.py --individuals 16 --crossover-two-points --no-random-mutation  > results/results_16_2_one_$1.txt
+
+	# python3 genetic.py --individuals 32 --crossover-one-point --random-mutation		> results/results_32_1_random_$1.txt
+	# python3 genetic.py --individuals 32 --crossover-one-point --no-random-mutation		> results/results_32_1_one_$1.txt
+	# python3 genetic.py --individuals 32 --crossover-two-points --random-mutation		> results/results_32_2_random_$1.txt
+	# python3 genetic.py --individuals 32 --crossover-two-points --no-random-mutation		> results/results_32_2_one_$1.txt
+
+	# python3 genetic.py --individuals 64 --crossover-one-point --random-mutation	> results/results_64_1_random_$1.txt
+	# python3 genetic.py --individuals 64 --crossover-one-point --no-random-mutation	> results/results_64_1_one_$1.txt
+	# python3 genetic.py --individuals 64 --crossover-two-points --random-mutation	> results/results_64_2_random_$1.txt
+	# python3 genetic.py --individuals 64 --crossover-two-points --no-random-mutation	> results/results_64_2_one_$1.txt
+
 
 
 done
