64d45db9238d8862b928a3458c8a1f902bc01fa8
Ernesto Serrano <info@ernesto.es>
2019-11-05 19:22:54 +0000

    Optimized run.sh

--- code/run.sh
+++ code/run.sh
@@ -4,24 +4,15 @@
 # Enter as first parameter the number of executions
 # example: ./run.sh 30 for 30 executions
 
+INDIVIDUALS=16
 
 for i in `seq $1` ; do
 	echo "Running $i execution..."
     
-    python3 genetic.py --individuals 16 --crossover-one-point --random-mutation > results/results_16_1_random_$i.txt
-	python3 genetic.py --individuals 16 --crossover-one-point --no-random-mutation > results/results_16_1_one_$i.txt
-	python3 genetic.py --individuals 16 --crossover-two-points --random-mutation > results/results_16_2_random_$i.txt
-	python3 genetic.py --individuals 16 --crossover-two-points --no-random-mutation  > results/results_16_2_one_$i.txt
-
-	# python3 genetic.py --individuals 32 --crossover-one-point --random-mutation		> results/results_32_1_random_$i.txt
-	# python3 genetic.py --individuals 32 --crossover-one-point --no-random-mutation		> results/results_32_1_one_$i.txt
-	# python3 genetic.py --individuals 32 --crossover-two-points --random-mutation		> results/results_32_2_random_$i.txt
-	# python3 genetic.py --individuals 32 --crossover-two-points --no-random-mutation		> results/results_32_2_one_$i.txt
-
-	# python3 genetic.py --individuals 64 --crossover-one-point --random-mutation	> results/results_64_1_random_$i.txt
-	# python3 genetic.py --individuals 64 --crossover-one-point --no-random-mutation	> results/results_64_1_one_$i.txt
-	# python3 genetic.py --individuals 64 --crossover-two-points --random-mutation	> results/results_64_2_random_$i.txt
-	# python3 genetic.py --individuals 64 --crossover-two-points --no-random-mutation	> results/results_64_2_one_$i.txt
+    python3 genetic.py --individuals $INDIVIDUALS --crossover-one-point --random-mutation > results/results_$INDIVIDUALS_1_random_$i.txt
+	python3 genetic.py --individuals $INDIVIDUALS --crossover-one-point --no-random-mutation > results/results_$INDIVIDUALS_1_one_$i.txt
+	python3 genetic.py --individuals $INDIVIDUALS --crossover-two-points --random-mutation > results/results_$INDIVIDUALS_2_random_$i.txt
+	python3 genetic.py --individuals $INDIVIDUALS --crossover-two-points --no-random-mutation  > results/results_$INDIVIDUALS_2_one_$i.txt
 
 done
 echo "Finised!"
